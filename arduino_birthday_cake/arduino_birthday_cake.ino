78f1b5a4a91e9b0729f51bdba9523701ff3820ae
Carl Marquez <carl.louis.marquez@gmail.com>
2016-11-17 23:45:59 -0500

    Added Serial communication between Arduino and PC
    Added a line of code in the Arduino sketch that sends a byte through the
    Serial Port, created a program in Java that receives the byte

--- arduino_birthday_cake/arduino_birthday_cake.ino
+++ arduino_birthday_cake/arduino_birthday_cake.ino
@@ -1,19 +1,27 @@
 const int piezoPin = A0;
 const int ledPin = 2;
 int piezoVal = 0;
-int ledVal = HIGH;
+int currentLedVal = HIGH;
+int previousLedVal = HIGH;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, ledVal);
+  digitalWrite(ledPin, currentLedVal);
   piezoVal = analogRead(piezoPin);
-  if (piezoVal > 1) {
+  if (piezoVal > 2) {
     // Turns off the LEDs if the piezo detects a vibration
-    ledVal = LOW;
+    currentLedVal = LOW;
   }
+  if (currentLedVal != previousLedVal && currentLedVal == LOW) {
+    // If the sketch detects a state change from HIGH to LOW,
+    // it sends a byte 5 through the serial port
+    Serial.write(5);
+  }
+  previousLedVal = currentLedVal;
 }
