f38cf595489247ead3eaf20c2dd73cd26f9dfbff
Carl Marquez <carl.louis.marquez@gmail.com>
2016-11-19 22:41:16 -0500

    Added images and documentation

--- ArduinoBirthdayCakeDisplay/src/com/cmjten/arduino/bdaydisplay/BDayDisplay.java
+++ ArduinoBirthdayCakeDisplay/src/com/cmjten/arduino/bdaydisplay/BDayDisplay.java
@@ -1,13 +1,23 @@
 package com.cmjten.arduino.bdaydisplay;
 
 import java.awt.*;
+import java.io.IOException;
 import javax.swing.*;
 
 /**
- * The frame for the Birthday Display
+ * Arduino Uno Birthday Cake
+ * 
+ * The Arduino Uno sketch simulates a birthday cake by using a piezo and LEDs.
+ * When the user blows on the piezo, the LEDs turn off. The sketch then sends a 
+ * signal to this Java program through the serial port to display a birthday
+ * message. This Java program is also able to send a signal back to the Arduino
+ * Uno to relight the LEDs.
+ * 
+ * External libraries required:
+ * - jSerialComm by Fazecast: https://github.com/Fazecast/jSerialComm (jar file in lib folder)
  */
 public class BDayDisplay extends JFrame {
-
+	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -18,11 +28,14 @@ public class BDayDisplay extends JFrame {
 	
 	/**
 	 * Creates the frame of the program and adds the components
+	 * @throws IOException 
 	 */
 	public BDayDisplay() {
+		this.setTitle("Arduino Uno Cake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-		this.setSize(480, 360);
+		this.setSize(480, 400);
+		this.setResizable(false);
 		
 		// The panel on which the message will be displayed
 		this.add(new DisplayPanel(new DisplayController()), BorderLayout.CENTER);
--- ArduinoBirthdayCakeDisplay/src/com/cmjten/arduino/bdaydisplay/DisplayPanel.java
+++ ArduinoBirthdayCakeDisplay/src/com/cmjten/arduino/bdaydisplay/DisplayPanel.java
@@ -2,8 +2,10 @@ package com.cmjten.arduino.bdaydisplay;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
+import java.awt.image.*;
+import java.io.*;
 import java.util.*;
+import javax.imageio.*;
 import javax.swing.*;
 
 /**
@@ -12,6 +14,9 @@ import javax.swing.*;
 public class DisplayPanel extends JPanel implements Observer, MouseListener {
 	
 	private DisplayController displayController;
+	private BufferedImage birthdayMessage;
+	private BufferedImage blowMessage;
+	private BufferedImage noPortMessage;
 	
 	/**
 	 * Constructor
@@ -21,6 +26,21 @@ public class DisplayPanel extends JPanel implements Observer, MouseListener {
 		this.displayController = displayController;
 		this.displayController.addObserver(this);
 		this.addMouseListener(this);
+		
+		try {
+			// Gets the images required by the program
+			this.birthdayMessage = ImageIO.read(new File("images/cake.png"));
+			this.blowMessage = ImageIO.read(new File("images/blow_on_the_cake.png"));
+			this.noPortMessage = ImageIO.read(new File("images/no_port_detected.png"));
+		} 
+		catch (IOException e) {
+			// If at least one of the images are not found, sets all instance
+			// variables for the images to null
+			this.birthdayMessage = null;
+			this.blowMessage = null;
+			this.noPortMessage = null;
+			System.out.println("One or more image file(s) not found");
+		}
 	}
 	
 	/**
@@ -31,20 +51,15 @@ public class DisplayPanel extends JPanel implements Observer, MouseListener {
 		// Allows usage of advanced graphics library
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;	// Advanced Graphics library
+		this.setBackground(Color.WHITE);
 		
-		// ***** CHANGE LATER *****
-		if (!this.displayController.getIsLit()) {
-			// After blowing the candles, displays the message and the amount of time
-			// the candles have been blown (temporary)
-			this.setBackground(Color.WHITE);
-			g2d.drawString("Happy Birthday!", 0, 10);
-			
-			String timesBlownString = "Times blown: " + this.displayController.getTimesBlown();
-			g2d.drawString(timesBlownString, 0, 25);
+		if (this.birthdayMessage != null) {
+			// No need to check if the other messages are not null because it's
+			// either all images are found or all are null
+			this.displayImages(g2d);
 		}
 		else {
-			// If the candles are lit, displays a black background (temporary)
-			this.setBackground(Color.BLACK);
+			this.displayText(g2d);
 		}
 	}
 	
@@ -59,6 +74,52 @@ public class DisplayPanel extends JPanel implements Observer, MouseListener {
 	 */
 	public DisplayController getDisplayController() {
 		return this.displayController;
+	}
+	
+	/**
+	 * Displays the images if the image files exist
+	 * @param g2d	Graphics library
+	 */
+	public void displayImages(Graphics2D g2d) {
+		if (this.displayController.getSerialPort() == null) {
+			// No port detected
+			g2d.drawImage(this.noPortMessage, 0, 0, 480, 360, null);
+		}
+		else if (!this.displayController.getIsLit()) {
+			// After blowing the candles, displays the birthday message and the 
+			// number of times the candles have been blown 
+			g2d.drawImage(this.birthdayMessage, 0, 0, 480, 360, null);
+			
+			String timesBlown = "Times blown: " + this.displayController.getTimesBlown();
+			g2d.drawString(timesBlown, 5, 360);
+		}
+		else {
+			// If the candles are lit, displays a message to blow the candles
+			g2d.drawImage(this.blowMessage, 0, 0, 480, 360, null);
+		}
+	}
+	
+	/**
+	 * Displays text if at least one of the image files does not exist
+	 * @param g2d	Graphics library
+	 */
+	public void displayText(Graphics2D g2d) {
+		if (this.displayController.getSerialPort() == null) {
+			// No port detected
+			g2d.drawString("No port detected", 5, 360);
+		}
+		else if (!this.displayController.getIsLit()) {
+			// After blowing the candles, displays the birthday message and the 
+			// number of times the candles have been blown 
+			g2d.drawString("Happy birthday!", 5, 340);
+			
+			String timesBlown = "Times blown: " + this.displayController.getTimesBlown();
+			g2d.drawString(timesBlown, 5, 360);
+		}
+		else {
+			// If the candles are lit, displays a message to blow the candles
+			g2d.drawString("Blow on the cake", 5, 360);
+		}
 	}
 
 	@Override
@@ -87,7 +148,8 @@ public class DisplayPanel extends JPanel implements Observer, MouseListener {
 		// TODO Auto-generated method stub
 		try {
 			this.displayController.light();
-		} catch (IOException e1) {
+		} 
+		catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
